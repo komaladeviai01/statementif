79b2a0977ebdce812f3c2a0538e86b7a08b98029
komaladeviai01 <komaladeviai01@gmail.com>
2024-01-04 13:28:02 +0530

    added ifstatement.c file

---
+++ ifstatement.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int  main()
+{
+    int a;
+    {
+        printf("enter the values......");
+        scanf("%d",&a);
+    }
+   if(a>0)
+    {
+        printf("positive values......");
+    }
+    else 
+    {
+        printf("nagative values ......");
+    }
+    return 0;
+}
